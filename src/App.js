7bb0c33eabe80084fad3ac922cab736d9cd558bd
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2019-12-06 07:38:09 +0100

    try commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import Header from './containers/Header'
-import Footer from './containers/Footer'
-import Home from './views/Home'
-import Form from './views/Form'
-import Event from './views/Event'
-import Auth from './views/Auth/Auth'
-import { AppConfig } from './config';
-
+import React from "react";
+import { Route } from "react-router-dom";
+import Header from "./containers/Header";
+import Footer from "./containers/Footer";
+import Home from "./views/Home";
+import Form from "./views/Form";
+import Event from "./views/Event";
+import Auth from "./views/Auth/Auth";
+import { AppConfig } from "./config";
+//cool
 const App = () => (
   <div>
     <Header />
 
     <div className="container" id="main">
-      { AppConfig.projectUrl === 'https://reactstarter.form.io' ?
-        <div className="alert alert-warning">This app is still configured to use the default project. Be sure to create your own project in form.io and change the PROJECT_URL in src/config.js</div>
-        : null
-      }
+      {AppConfig.projectUrl === "https://reactstarter.form.io" ? (
+        <div className="alert alert-warning">
+          This app is still configured to use the default project. Be sure to
+          create your own project in form.io and change the PROJECT_URL in
+          src/config.js
+        </div>
+      ) : null}
       <Route exact path="/" component={Home} />
       <Route path="/form" component={Form} />
       <Route path="/event" component={Event} />
@@ -25,6 +28,6 @@ const App = () => (
 
     <Footer />
   </div>
-)
+);
 
-export default App
+export default App;
